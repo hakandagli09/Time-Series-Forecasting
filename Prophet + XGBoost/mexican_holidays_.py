fddbfe288861658a6ab36fc285f1b293ab37e585
Hakan Dagli <163216642+hakandagli09@users.noreply.github.com>
2025-02-11 18:03:41 -0500

    Add files via upload

---
+++ Prophet + XGBoost/mexican_holidays_.py
@@ -0,0 +1,94 @@
+mexican_holidays = [
+    # 2019
+    # New Year's Day
+    '2019-04-21',  # Easter Sunday
+    '2019-05-05',  # Labor Day
+    '2019-07-07',  # Start of Summer Vacation
+    '2019-07-28',  # End of Summer Vacation
+    '2019-09-15',  # Independence Day
+    '2019-12-22',  # Christmas Day
+    '2019-01-06',  # New Years Day
+    
+    # 2020
+    '2020-04-12',  # Easter Sunday
+    '2020-05-03',  # Labor Day
+    '2020-07-05',  # Start of Summer Vacation
+    '2020-07-26',  # End of Summer Vacation
+    '2020-09-13',  # Independence Day
+    '2020-12-20',  # Christmas Day
+    '2020-01-05',  # New Years Day
+
+    
+    # 2021
+    '2021-01-03',  # New Year's Day
+    '2021-04-04',  # Easter Sunday
+    '2021-04-25',  # Labor Day
+    '2021-07-04',  # Start of Summer Vacation
+    '2021-07-25',  # End of Summer Vacation
+    '2021-09-12',  # Independence Day
+    '2021-12-19',  # Christmas Day
+    
+    # 2022
+    '2022-01-02',  # New Year's Day
+    '2022-04-17',  # Easter Sunday
+    '2022-05-01',  # Labor Day
+    '2022-07-03',  # Start of Summer Vacation
+    '2022-07-31',  # End of Summer Vacation
+    '2022-09-11',  # Independence Day
+    '2022-12-25',  # Christmas Day
+    
+    # 2023
+    '2023-01-01',  # New Year's Day
+    '2023-04-09',  # Easter Sunday
+    '2023-04-30',  # Labor Day
+    '2023-07-02',  # Start of Summer Vacation
+    '2023-07-30',  # End of Summer Vacation
+    '2023-09-17',  # Independence Day
+    '2023-12-31',  # Christmas Day
+    
+    # 2024
+    '2024-01-07',  # New Year's Day
+    '2024-03-31',  # Easter Sunday
+    '2024-05-05',  # Labor Day
+    '2024-07-07',  # Start of Summer Vacation
+    '2024-07-28',  # End of Summer Vacation
+    '2024-09-15',  # Independence Day
+    '2024-12-29',  # Christmas Day
+    
+    
+    # 2025
+    '2025-01-05',  # New Year's Day
+    '2025-04-20',  # Easter Sunday
+    '2025-05-04',  # Labor Day
+    '2025-07-06',  # Start of Summer Vacation
+    '2025-07-27',  # End of Summer Vacation
+    '2025-09-14',  # Independence Day
+    '2025-12-28',  # Christmas Day
+
+    # 2026
+    '2026-01-04',  # New Year's Day
+    '2026-04-05',  # Easter Sunday
+    '2026-05-01',  # Labor Day
+    '2026-07-05',  # Start of Summer Vacation
+    '2026-07-26',  # End of Summer Vacation
+    '2026-09-13',  # Independence Day
+    '2026-12-27',  # Christmas Day
+
+    # 2027
+    '2027-01-03',  # New Year's Day
+    '2027-03-28',  # Easter Sunday
+    '2027-05-02',  # Labor Day
+    '2027-07-04',  # Start of Summer Vacation
+    '2027-07-25',  # End of Summer Vacation
+    '2027-09-12',  # Independence Day
+    '2027-12-26',  # Christmas Day
+
+    # 2028
+    '2028-01-02',  # New Year's Day
+    '2028-04-16',  # Easter Sunday
+    '2028-04-30',  # Labor Day
+    '2028-07-02',  # Start of Summer Vacation
+    '2028-07-30',  # End of Summer Vacation
+    '2028-09-17',  # Independence Day
+    '2028-12-31',  # Christmas Day
+]
